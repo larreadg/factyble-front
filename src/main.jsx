7260185d4eae22e00347a091de864bf8b49c41cf
Diego Larrea <larreadi@personal.com.py>
2024-07-27 14:40:07 -0400

    Untrack files in .gitignore

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import { NextUIProvider } from '@nextui-org/react'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
